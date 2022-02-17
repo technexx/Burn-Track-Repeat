f70872cd7e6576b8a23ef4177c4b32b3c5c07154
technexx <48839830+technexx@users.noreply.github.com>
2022-02-17 12:23:05 -0800

    Daily Stats db methods moved to sep. class file, which implements Main in order to retrieve selected activity

--- app/src/main/java/com/example/tragic/irate/simple/stopwatch/DailyStatsFragment.java
+++ app/src/main/java/com/example/tragic/irate/simple/stopwatch/DailyStatsFragment.java
@@ -8,52 +8,46 @@ import android.widget.CalendarView;
 import android.widget.DatePicker;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class DailyStatsFragment extends Fragment {
 
-//    DatePicker datePicker;
     CalendarView calendarView;
     int dayOfMonthSelected;
     int monthSelected;
     int yearSelected;
 
-    TextView dailyStatsHeader;
     RecyclerView dailyStatsRecyclerview;
 
     TextView dailyStatsTotalSetTimeTextView;
     TextView dailyStatsTotalBreakTimeTextView;
     TextView dailyStatsTotalCaloriesBurnedTextView;
 
-    //Todo: Should have recyclerView for multiple activities in day.
-    //Todo: Totals for day should also be in recyclerView as last row. That way, they will automatically place themselves at bottom and fully aligned.
-
     public View onCreateView(final LayoutInflater inflater, final ViewGroup container, final Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.daily_stats_fragment_layout, container, false);
 
-//        datePicker = root.findViewById(R.id.stats_date_picker);
         calendarView = root.findViewById(R.id.stats_calendar);
-        dailyStatsHeader = root.findViewById(R.id.daily_stats_header);
         dailyStatsRecyclerview = root.findViewById(R.id.daily_stats_recyclerView);
 
         dailyStatsTotalSetTimeTextView = root.findViewById(R.id.daily_stats_total_set_time_textView);
         dailyStatsTotalBreakTimeTextView = root.findViewById(R.id.daily_stats_total_break_time_textView);
         dailyStatsTotalCaloriesBurnedTextView = root.findViewById(R.id.daily_stats_total_calories_burned_textView);
 
-        testCalendar();
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+
+            }
+        });
 
         return root;
     }
 
-    private void testCalendar() {
-        CalendarValues calendarValues = new CalendarValues();
-        String date = calendarValues.getDateString();
 
-        dailyStatsHeader.setText(date);
+    private void assignTotalTimeAndCaloriesForSelectedDayToTextView() {
 
-        yearSelected = calendarValues.getYearSelected();
-        monthSelected = calendarValues.getMonthSelected();
-        dayOfMonthSelected = calendarValues.getDayOfMonthSelected();
     }
 }
--- app/src/main/java/com/example/tragic/irate/simple/stopwatch/MainActivity.java
+++ app/src/main/java/com/example/tragic/irate/simple/stopwatch/MainActivity.java
@@ -800,10 +800,7 @@ public class MainActivity extends AppCompatActivity implements SavedCycleAdapter
     });
 
     fab.setOnClickListener(v -> {
-//      fabLogic();
-//      AsyncTask.execute(insertTotalTimesAndCaloriesBurnedOfCurrentDayIntoDatabase());
-      AsyncTask.execute(insertTotalTimesAndCaloriesForEachActivityWithinASpecificDayRunnable());
-
+      fabLogic();
     });
 
     stopwatch.setOnClickListener(v-> {
@@ -4545,7 +4542,7 @@ public class MainActivity extends AppCompatActivity implements SavedCycleAdapter
     }
   }
 
-  private String getTdeeActivityStringFromSavedArrayPosition() {
+  public String getTdeeActivityStringFromSavedArrayPosition() {
     ArrayList<String[]> subCategoryArray = tDEEChosenActivitySpinnerValues.subCategoryListOfStringArrays;
     String[] subCategoryList = subCategoryArray.get(selectedTdeeCategoryPosition);
     return (String) subCategoryList[selectedTdeeSubCategoryPosition];
@@ -4610,192 +4607,6 @@ public class MainActivity extends AppCompatActivity implements SavedCycleAdapter
     String[] valueArray = tDEEChosenActivitySpinnerValues.subValueListOfStringArrays.get(selectedTdeeCategoryPosition);
     double preRoundedMet = Double.parseDouble(valueArray[selectedTdeeValuePosition]);
     return preRoundedMet;
-  }
-
-  private Runnable insertTotalTimesAndCaloriesBurnedOfCurrentDayIntoDatabase() {
-    return new Runnable() {
-      @Override
-      public void run() {
-        String date = calendarValues.getDateString();
-        int dayOfYear = calendar.get(Calendar.DAY_OF_YEAR);
-
-        //Check if current day already exists in db.
-        List<DayHolder> dayHolderList = cyclesDatabase.cyclesDao().loadAllDayHolderRows();
-        int dayHolderSize = dayHolderList.size();
-
-        for (int i=0; i<dayHolderSize; i++) {
-          long dayInRow = dayHolderList.get(i).getDayId();
-          if (dayOfYear==dayInRow) {
-            retrieveTotalTimesAndCaloriesBurnedOfCurrentDayFromDatabase();
-            return;
-          }
-        }
-
-        DayHolder dayHolder = new DayHolder();
-        ActivitiesForEachDay activitiesForEachDay = new ActivitiesForEachDay();
-
-        dayHolder.setDayId(dayOfYear);
-        dayHolder.setDate(date);
-
-        activitiesForEachDay.setUniqueDayIdPossessedByEachOfItsActivities(dayOfYear);
-        dayHolder.setTotalSetTime(0);
-        dayHolder.setTotalBreakTime(0);
-        dayHolder.setTotalCaloriesBurned(0);
-
-        cyclesDatabase.cyclesDao().insertDay(dayHolder);
-        cyclesDatabase.cyclesDao().insertActivitiesForEachDay(activitiesForEachDay);
-      }
-    };
-  }
-
-  private void retrieveTotalTimesAndCaloriesBurnedOfCurrentDayFromDatabase() {
-    int dayOfYear = calendar.get(Calendar.DAY_OF_YEAR);
-    //Always a single row return, since only one exists per day of year.
-    List<DayHolder> dayHolderList = cyclesDatabase.cyclesDao().loadSingleDay(dayOfYear);
-
-    totalSetTimeForCurrentDayInMillis = dayHolderList.get(0).getTotalSetTime();
-    totalBreakTimeForCurrentDayInMillis = dayHolderList.get(0).getTotalBreakTime();
-    totalCaloriesBurnedForCurrentDay = dayHolderList.get(0).getTotalCaloriesBurned();
-  }
-
-  private Runnable updateTotalTimesAndCaloriesBurnedForCurrentDayFromDatabaseRunnable() {
-    return new Runnable() {
-      @Override
-      public void run() {
-        int dayOfYear = calendar.get(Calendar.DAY_OF_YEAR);
-        List<DayHolder> dayHolderList = cyclesDatabase.cyclesDao().loadAllDayHolderRows();
-        DayHolder dayHolder = dayHolderList.get(dayOfYear);
-
-        dayHolder.setTotalSetTime(totalSetTimeForCurrentDayInMillis);
-        dayHolder.setTotalBreakTime(totalBreakTimeForCurrentDayInMillis);
-        dayHolder.setTotalCaloriesBurned(totalCaloriesBurnedForCurrentDay);
-
-        cyclesDatabase.cyclesDao().updateDayHolder(dayHolder);
-      }
-    };
-  }
-
-  //Since DayHolder's dayId and CycleStat's setUniqueDayIdPossessedByEachOfItsActivities are identical, we simply tie StatsForEachActivityWithinCycle's unique ID to that as well.
-  private Runnable insertTotalTimesAndCaloriesForEachActivityWithinASpecificDayRunnable() {
-    return new Runnable() {
-      @Override
-      public void run() {
-        int dayOfYear = calendar.get(Calendar.DAY_OF_YEAR);
-
-        //Retrieves for activities tied to specific date ID, since we only want to check against the activities selected for current day.
-        List<StatsForEachActivity> statsForEachActivityList = cyclesDatabase.cyclesDao().loadActivitiesForSpecificDate(dayOfYear);
-
-        for (int i=0; i<statsForEachActivityList.size(); i++) {
-          if (getTdeeActivityStringFromSavedArrayPosition().equals(statsForEachActivityList.get(i).getActivity())) {
-            activityPositionInDb = i;
-            retrieveTotalTimesAndCaloriesForActivityWithinASpecificDayRunnable(activityPositionInDb);
-            return;
-          }
-        }
-
-        StatsForEachActivity statsForEachActivity = new StatsForEachActivity();
-        statsForEachActivity.setUniqueIdTiedToTheSelectedActivity(dayOfYear);
-        statsForEachActivity.setActivity(getTdeeActivityStringFromSavedArrayPosition());
-
-
-        statsForEachActivity.setTotalSetTimeForEachActivity(0);
-        statsForEachActivity.setTotalBreakTimeForEachActivity(0);
-        statsForEachActivity.setTotalCaloriesBurnedForEachActivity(0);
-
-        cyclesDatabase.cyclesDao().insertStatsForEachActivityWithinCycle(statsForEachActivity);
-      }
-    };
-  }
-
-  private void retrieveTotalTimesAndCaloriesForActivityWithinASpecificDayRunnable(int activityPosition) {
-    List<StatsForEachActivity> statsForEachActivityList = cyclesDatabase.cyclesDao().loadActivitiesForSpecificDate(activityPosition);
-    StatsForEachActivity statsForEachActivity = statsForEachActivityList.get(0);
-
-  }
-
-  private Runnable updateTotalTimesAndCaloriesBurnedForSpecificActivityOnSpecificDayRunnable() {
-    return new Runnable() {
-      @Override
-      public void run() {
-        int dayOfYear = calendar.get(Calendar.DAY_OF_YEAR);
-
-        //This retrieves all rows tied key'd to the day we have selected.
-        List<StatsForEachActivity> statsList = cyclesDatabase.cyclesDao().loadActivitiesForSpecificDate(dayOfYear);
-
-        StatsForEachActivity statsForEachActivity = new StatsForEachActivity();
-        if (statsList.size() >= activityPositionInDb+1) {
-           statsForEachActivity = statsList.get(activityPositionInDb);
-        } else {
-          Log.e("Stats Database", "Activity position in StatsForEachActivityWithinCycle does not exist!");
-        }
-
-        statsForEachActivity.setTotalSetTimeForEachActivity(totalSetTimeForSpecificActivityForCurrentDayInMillis);
-        statsForEachActivity.setTotalBreakTimeForEachActivity(totalBreakTimeForSpecificActivityForCurrentDayInMillis);
-        statsForEachActivity.setTotalCaloriesBurnedForEachActivity(totalCaloriesBurnedForSpecificActivityForCurrentDay);
-        cyclesDatabase.cyclesDao().updateStatsForEachActivity(statsForEachActivity);
-      }
-    };
-  }
-
-  private void testStatsForEachActivityInsertion() {
-    AsyncTask.execute(new Runnable() {
-      @Override
-      public void run() {
-        StatsForEachActivity statsForEachActivity = new StatsForEachActivity();
-        List<StatsForEachActivity> statsForEachActivityList = cyclesDatabase.cyclesDao().loadAllActivitiesForAllDays();
-
-        statsForEachActivity.setActivity("test" + statsForEachActivityList.size());
-        cyclesDatabase.cyclesDao().insertStatsForEachActivityWithinCycle(statsForEachActivity);
-
-        Log.i("testDb", "number of activities added is " + statsForEachActivityList.size());
-        for (int i=0; i<statsForEachActivityList.size(); i++) {
-          Log.i("testDb", "activity name is " + statsForEachActivityList.get(i).getActivity());
-          Log.i("testDb", "unique ID is " + statsForEachActivityList.get(i).getUniqueIdTiedToTheSelectedActivity());
-        }
-      }
-    });
-  }
-
-  private void testStatsForDayInsertion(boolean changingDate) {
-    AsyncTask.execute(new Runnable() {
-      @Override
-      public void run() {
-        int dayOfYearChangingTestValue = calendar.get(Calendar.DAY_OF_YEAR);
-
-        List<DayHolder> dayHolderList = cyclesDatabase.cyclesDao().loadAllDayHolderRows();
-        int dayHolderSize = dayHolderList.size();
-
-        for (int i=0; i<dayHolderSize; i++) {
-          if (changingDate) {
-            dayOfYearChangingTestValue = 10*i;
-          }
-
-          long dayInRow = dayHolderList.get(i).getDayId();
-          if (dayInRow==dayOfYearChangingTestValue) {
-            long dayID = cyclesDatabase.cyclesDao().loadAllDayHolderRows().get(i).getDayId();
-            Log.i("testdb", "dayholder contains dates " + dayID);
-          }
-        }
-      }
-    });
-  }
-
-  private void logTotalTimesForSelectedDay() {
-    Log.i("testStats", "total set time is " + totalSetTimeForCurrentDayInMillis);
-    Log.i("testStats", "total break time is " + totalBreakTimeForCurrentDayInMillis);
-  }
-
-  private void logTotalCaloriesForSelectedDay() {
-    Log.i("testStats", "total calories time are " + totalCaloriesBurnedForCurrentDay);
-  }
-
-  private void logTotalTimesForSpecificActivityOnSelectedDay() {
-    Log.i("testStats", "total set time for activity is " + totalSetTimeForSpecificActivityForCurrentDayInMillis);
-    Log.i("testStats", "total break time for activity is " + totalBreakTimeForSpecificActivityForCurrentDayInMillis);
-  }
-
-  private void logTotalCaloriesForSpecificActivityOnSelectedDay() {
-    Log.i("testStats", "total calories for activity are " + totalCaloriesBurnedForSpecificActivityForCurrentDay);
   }
 
   private void logTdeeCategoryPositions() {
