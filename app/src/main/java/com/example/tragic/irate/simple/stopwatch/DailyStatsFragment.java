a6e8fae4e60be83e4a8664b668d076bb49428e02
technexx <48839830+technexx@users.noreply.github.com>
2022-05-17 17:53:09 -0700

    layout adjustment for stats

--- app/src/main/java/com/example/tragic/irate/simple/stopwatch/DailyStatsFragment.java
+++ app/src/main/java/com/example/tragic/irate/simple/stopwatch/DailyStatsFragment.java
@@ -594,6 +594,7 @@ public class DailyStatsFragment extends Fragment implements DailyStatsAdapter.td
         totalStatsHeaderTextView = mRoot.findViewById(R.id.total_stats_header);
         statsTotalSetTimeTextView = mRoot.findViewById(R.id.daily_stats_total_set_time_textView);
         statsTotalBreakTimeTextView = mRoot.findViewById(R.id.daily_stats_total_break_time_textView);
+        statsTotalBreakTimeTextView.setVisibility(View.INVISIBLE);
         statsTotalCaloriesBurnedTextView = mRoot.findViewById(R.id.daily_stats_total_calories_burned_textView);
         statDurationSwitcherButtonLeft = mRoot.findViewById(R.id.stat_duration_switcher_button_left);
         statDurationSwitcherButtonRight = mRoot.findViewById(R.id.stat_duration_switcher_button_right);
