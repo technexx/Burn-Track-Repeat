1a3391e47334111fbfcb7c8203e9e323c34b224d
technexx <48839830+technexx@users.noreply.github.com>
2022-06-21 11:21:24 -0700

    more exp layout changes

--- app/src/main/java/com/example/tragic/irate/simple/stopwatch/Database/DailyStatClasses/DailyStatsFragment.java
+++ app/src/main/java/com/example/tragic/irate/simple/stopwatch/Database/DailyStatClasses/DailyStatsFragment.java
@@ -933,7 +933,7 @@ public class DailyStatsFragment extends Fragment implements DailyStatsAdapter.td
 
     private void instantiateExpansionPopUpViews() {
         dailyStatsExpandedView = inflater.inflate(R.layout.daily_stats_expanded_popup, null);
-        dailyStatsExpandedPopUpWindow = new PopupWindow(dailyStatsExpandedView, WindowManager.LayoutParams.MATCH_PARENT, dpToPxConv(315), false);
+        dailyStatsExpandedPopUpWindow = new PopupWindow(dailyStatsExpandedView, WindowManager.LayoutParams.MATCH_PARENT, dpToPxConv(325), false);
         dailyStatsExpandedPopUpWindow.setAnimationStyle(R.style.SlideFromLeftAnimationShort);
 
         expansionAssignedHeader = dailyStatsExpandedView.findViewById(R.id.expansion_assigned_header);
